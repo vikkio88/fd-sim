e3110ddfd326f56eb18e8bf85b0171c46a1d2a5e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-06-04 22:12:30 +0100

    adding free agents

--- main.go
+++ main.go
@@ -19,13 +19,17 @@ func main() {
 		}
 
 		fmt.Printf("players#: %d\n", db.PlayerR().Count())
+		fmt.Printf("free agents#: %d\n", len(db.PlayerR().FreeAgents()))
 		return
 	}
 
 	fmt.Println("Had no teams, generating")
 	tg := generators.NewTeamGen(time.Now().Unix())
-	ts := tg.Teams(40)
+	ts := tg.Teams(20)
 	db.TeamR().Insert(ts)
+	pg := generators.NewPeopleGen(time.Now().Unix())
+	ps := pg.Players(4)
+	db.PlayerR().Insert(ps)
 
 	// a := ui.NewApp()
 	// a.Run()
