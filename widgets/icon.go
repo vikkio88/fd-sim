6ce8dc377f37c972a1f237d57f050159fa1622e7
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-06-08 19:45:26 +0100

    implemented first icons poc

---
+++ widgets/icon.go
@@ -0,0 +1,30 @@
+package widgets
+
+import (
+	"fdsim/res"
+
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+func Icon(name string) *widget.Icon {
+	switch name {
+	//TODO: move those to enums
+	case "city":
+		return widget.NewIcon(theme.NewThemedResource(res.City))
+	case "dumbell":
+		return widget.NewIcon(theme.NewThemedResource(res.Dumbell))
+	case "team":
+		return widget.NewIcon(theme.NewThemedResource(res.Team))
+	case "money":
+		return widget.NewIcon(theme.NewThemedResource(res.Money))
+	case "sad_face":
+		return widget.NewIcon(theme.NewThemedResource(res.SadFace))
+	case "meh_face":
+		return widget.NewIcon(theme.NewThemedResource(res.MehFace))
+	case "happy_face":
+		return widget.NewIcon(theme.NewThemedResource(res.HappyFace))
+	}
+
+	return widget.NewIcon(theme.NewThemedResource(res.City))
+}
