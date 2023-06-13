b23e460024f08c22f393662b7303b3f3859ec555
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-06-13 21:33:27 +0100

    adding Coach rel const

--- db/consts.go
+++ db/consts.go
@@ -5,6 +5,7 @@ const moneyMultiplier = 100
 // Relations
 const teamsRel = "Teams"
 const teamsAndPlayersRel = "Teams.Players"
+const teamsAndCoachRel = "Teams.Coach"
 const roundsRel = "Rounds"
 const roundsAndMatchesRel = "Rounds.Matches"
 const tableRowsRel = "TableRows"
