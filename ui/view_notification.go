b8e47d69c1d28264de6e84a57fda5f8ed7ee497c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-06-24 20:49:03 +0100

    adding notification scaffolded card for email/news

--- ui/view_notification.go
+++ ui/view_notification.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"fdsim/conf"
 	"fmt"
 
 	"fyne.io/fyne/v2"
@@ -10,13 +11,41 @@ import (
 
 func notificationView(ctx *AppContext, route AppRoute) *fyne.Container {
 	id := ctx.RouteParam.(string)
+	var content fyne.CanvasObject
+	var title string
+	if route == Email {
+		title = "Email"
+		content = makeEmail(id, ctx)
+	} else {
+		title = "News"
+		content = makeNews(id, ctx)
+	}
+
 	return NewFborder().
-		Top(NewFborder().Left(backButton(ctx)).Get(centered(h1(route.String())))).
+		Top(NewFborder().Left(backButton(ctx)).Get(centered(h1(title)))).
 		Get(
-			container.NewCenter(
-				widget.NewLabel(
-					fmt.Sprintf("Notification %s", id),
-				),
-			),
+			content,
 		)
+}
+
+func makeNews(id string, ctx *AppContext) fyne.CanvasObject {
+	news := ctx.Db.GameR().GetNewsById(id)
+	return container.NewMax(
+		widget.NewCard(
+			news.Title,
+			fmt.Sprintf("%s - %s", news.Date.Format(conf.DateFormatShort), news.NewsPaper),
+			widget.NewRichText(&widget.TextSegment{Text: news.Body}),
+		),
+	)
+}
+
+func makeEmail(id string, ctx *AppContext) fyne.CanvasObject {
+	email := ctx.Db.GameR().GetEmailById(id)
+	return container.NewMax(
+		widget.NewCard(
+			email.Subject,
+			fmt.Sprintf("%s - %s", email.Date.Format(conf.DateFormatShort), email.Sender),
+			widget.NewRichText(&widget.TextSegment{Text: email.Body}),
+		),
+	)
 }
