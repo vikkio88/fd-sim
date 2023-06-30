aa71fdf5b790d8cb27092988422350a01b34aca2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-06-30 15:36:45 +0100

    adding Fame config

--- conf/conf.go
+++ conf/conf.go
@@ -1,11 +1,15 @@
 package conf
 
 const (
-	PlayerAgeRange  = "16..40"
-	CoachAgeRange   = "30..80"
+	PlayerAgeRange = "16..40"
+	CoachAgeRange  = "30..80"
+
 	DateFormatGame  = "Mon, 02 January 2006"
 	DateFormatShort = "02/01/2006"
-	StatsRowsLimit  = 10
+
+	StartingFame = 65
+
+	StatsRowsLimit = 10
 	// Placeholder for the link in the body of a notification
 	LinkBodyPH = "LINK"
 )
