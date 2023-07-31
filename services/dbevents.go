1e4b597136ad21d7bb5e2e14916161d216c86085
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-07-31 19:57:30 +0100

    moved events body to its own file

--- services/dbevents.go
+++ services/dbevents.go
@@ -2,9 +2,7 @@ package services
 
 import (
 	"encoding/json"
-	"fdsim/conf"
 	"fdsim/db"
-	"fdsim/enums"
 	"fdsim/models"
 	"fmt"
 )
@@ -31,61 +29,13 @@ func getEventFromDbEvent(dbe db.DbEventDto) *Event {
 
 	switch dbe.Type {
 	case db.DbEvPlRetiredFdTeam:
-		{
-			var retired []*models.PNPH
-			json.Unmarshal([]byte(dbe.Payload), &retired)
-
-			links := make([]models.Link, len(retired))
-			body := "The following players retired this year"
-			for i, rp := range retired {
-				links[i] = models.NewLink(rp.String(), enums.PlayerDetails, &rp.Id)
-				body += fmt.Sprintf(" %s", conf.LinkBodyPH)
-			}
-
-			event.TriggerEmail = models.NewEmail(
-				emailAddrFromTeamName(params.TeamName, "hr"),
-				fmt.Sprintf("Goodbye! %d Players retired.", len(retired)),
-				body,
-				dbe.TriggerDate,
-				links,
-			)
-			return event
-		}
+		return dbEvRetiredPlayers(dbe, event, params)
 	case db.DbEvPlayersSkillChanged:
-		{
-			var ps [2][]*models.PNPHVals
-			json.Unmarshal([]byte(dbe.Payload), &ps)
-			improved := ps[0]
-			worsened := ps[1]
-
-			links := []models.Link{}
-
-			body := "Few players this year showcased a significant change in their skills."
-			if len(improved) > 0 {
-				body += "\nThe following players improved quite a lot:"
-				for _, v := range improved {
-					links = append(links, models.NewLink(fmt.Sprintf("%s : +%d%%", v.String(), v.ValueI), enums.PlayerDetails, &v.Id))
-					body += fmt.Sprintf(" %s", conf.LinkBodyPH)
-				}
-			}
-			if len(worsened) > 0 {
-				body += "\nThose players skills appear to be worse:"
-				for _, v := range worsened {
-					links = append(links, models.NewLink(fmt.Sprintf("%s : -%d%%", v.String(), v.ValueI), enums.PlayerDetails, &v.Id))
-					body += fmt.Sprintf(" %s", conf.LinkBodyPH)
-				}
-			}
-
-			event.TriggerEmail = models.NewEmail(
-				emailAddrFromTeamName(params.TeamName, "training"),
-				"Report of your player skill change.",
-				body,
-				dbe.TriggerDate,
-				links,
-			)
-
-			return event
-		}
+		return dbEvSkillChangePlayers(dbe, event, params)
+	case db.DbEvPlayerLeftFreeAgent:
+		return dbEvFreeAgent(dbe, event, params)
+	case db.DbEvIndividualAwards:
+		return dbEvIndividualAwards(dbe, event, params)
 	}
 
 	return nil
---
+++ services/dbevents_bodies.go
@@ -0,0 +1,136 @@
+package services
+
+import (
+	"encoding/json"
+	"fdsim/conf"
+	"fdsim/data"
+	"fdsim/db"
+	"fdsim/enums"
+	"fdsim/models"
+	"fmt"
+)
+
+func parsePNPH(dbe db.DbEventDto) []*models.PNPH {
+	var result []*models.PNPH
+	json.Unmarshal([]byte(dbe.Payload), &result)
+	return result
+}
+
+func dbEvRetiredPlayers(dbe db.DbEventDto, event *Event, params models.EventParams) *Event {
+	retired := parsePNPH(dbe)
+
+	links := make([]models.Link, len(retired))
+	body := "The following players retired this year"
+	for i, rp := range retired {
+		links[i] = models.NewLink(rp.String(), enums.PlayerDetails, &rp.Id)
+		body += fmt.Sprintf(" %s", conf.LinkBodyPH)
+	}
+
+	event.TriggerEmail = models.NewEmail(
+		emailAddrFromTeamName(params.TeamName, "hr"),
+		fmt.Sprintf("Goodbye! %d Players retired.", len(retired)),
+		body,
+		dbe.TriggerDate,
+		links,
+	)
+	return event
+}
+
+func dbEvSkillChangePlayers(dbe db.DbEventDto, event *Event, params models.EventParams) *Event {
+	var ps [2][]*models.PNPHVals
+	json.Unmarshal([]byte(dbe.Payload), &ps)
+	improved := ps[0]
+	worsened := ps[1]
+
+	links := []models.Link{}
+
+	body := "Few players this year showcased a significant change in their skills."
+	if len(improved) > 0 {
+		body += "\nThe following players improved quite a lot:"
+		for _, v := range improved {
+			links = append(links, models.NewLink(fmt.Sprintf("%s : +%d%%", v.String(), v.ValueI), enums.PlayerDetails, &v.Id))
+			body += fmt.Sprintf(" %s", conf.LinkBodyPH)
+		}
+	}
+	if len(worsened) > 0 {
+		body += "\nThose players skills appear to be worse:"
+		for _, v := range worsened {
+			links = append(links, models.NewLink(fmt.Sprintf("%s : -%d%%", v.String(), v.ValueI), enums.PlayerDetails, &v.Id))
+			body += fmt.Sprintf(" %s", conf.LinkBodyPH)
+		}
+	}
+
+	event.TriggerEmail = models.NewEmail(
+		emailAddrFromTeamName(params.TeamName, "training"),
+		"Report of your player skill change.",
+		body,
+		dbe.TriggerDate,
+		links,
+	)
+
+	return event
+}
+
+func dbEvFreeAgent(dbe db.DbEventDto, event *Event, params models.EventParams) *Event {
+	freeagents := parsePNPH(dbe)
+
+	links := make([]models.Link, len(freeagents))
+	body := "The following players contracts were not renewed and they are now free agents:"
+	for i, rp := range freeagents {
+		links[i] = models.NewLink(rp.String(), enums.PlayerDetails, &rp.Id)
+		body += fmt.Sprintf(" %s", conf.LinkBodyPH)
+	}
+
+	event.TriggerEmail = models.NewEmail(
+		emailAddrFromTeamName(params.TeamName, "hr"),
+		fmt.Sprintf("Contracts expired for %d players.", len(freeagents)),
+		body,
+		dbe.TriggerDate,
+		links,
+	)
+	return event
+}
+
+func dbEvIndividualAwards(dbe db.DbEventDto, event *Event, params models.EventParams) *Event {
+	var statRows []*models.StatRow
+	json.Unmarshal([]byte(dbe.Payload), &statRows)
+
+	mvp := statRows[0]
+	topScorer := statRows[1]
+
+	links := []models.Link{
+		models.NewLink(mvp.Player.String(), enums.PlayerDetails, &mvp.Player.Id),
+		models.NewLink(mvp.Team.Name, enums.TeamDetails, &mvp.TeamId),
+		models.NewLink(topScorer.Player.String(), enums.PlayerDetails, &topScorer.Player.Id),
+		models.NewLink(topScorer.Team.Name, enums.TeamDetails, &topScorer.TeamId),
+	}
+	body := fmt.Sprintf(`The individual Awards for %s were assigned.
+
+	
+MVP
+%s %s
+ With an average score of %2.f in %d matches.
+
+
+Top Scorer
+%s %s
+ With %d goals in %d matches.`,
+		params.LeagueName,
+		conf.LinkBodyPH, //mvp link
+		conf.LinkBodyPH, // mvp team
+		mvp.Score/float64(mvp.Played),
+		mvp.Played,
+		conf.LinkBodyPH, // scorer link
+		conf.LinkBodyPH, // scorer team
+		topScorer.Goals, topScorer.Played,
+	)
+
+	event.TriggerNews = models.NewNews(
+		fmt.Sprintf("%s : Individual Awards Assigned!", params.LeagueName),
+		data.GetNewspaper(dbe.Country),
+		body,
+		dbe.TriggerDate,
+		links,
+	)
+	return event
+}
