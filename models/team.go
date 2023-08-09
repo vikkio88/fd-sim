73995687947d8b662f5810b7d6b100656ea8cab4
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-08-09 21:07:33 +0100

    started working on team accepting decision making

--- models/team.go
+++ models/team.go
@@ -4,6 +4,7 @@ import (
 	"fdsim/enums"
 	"fdsim/utils"
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/oklog/ulid/v2"
@@ -50,10 +51,36 @@ func NewTeam(name, city string, country enums.Country) *Team {
 	}
 }
 
+func (t *Team) AcceptsOffer(offer utils.Money, playerId string) utils.Perc {
+	p, ok := t.Roster.Player(playerId)
+	if !ok {
+		//TODO: log error no player in this team
+		return utils.NewPerc(0)
+	}
+
+	offerWeight := 0.3
+	valueWeight := 0.3
+	contractWeight := 0.2
+	balanceWageWeight := 0.2
+
+	normalizedOffer := offer.Value()
+	normalizedValue := p.Value.Value()
+	normalizedContract := float64(p.YContract) / 10
+	normalizedBalanceWage := t.Balance.Value() - t.Wages().Value()
+
+	weightedSum := (offerWeight * normalizedOffer) +
+		(valueWeight * normalizedValue) +
+		(contractWeight * normalizedContract) +
+		(balanceWageWeight * normalizedBalanceWage)
+
+	return utils.NewPerc(int(math.Round(weightedSum * 100)))
+}
+
 func (t *Team) Wages() utils.Money {
 	r := t.Roster.Wages()
 	return utils.NewEurosFromF(r.Value() + t.Coach.Wage.Value())
 }
+
 func (t *Team) TransferBudget() utils.Money {
 	val := t.Balance.Value() * t.TransferRatio
 	return utils.NewEurosFromF(val)
--- models/team_test.go
+++ models/team_test.go
@@ -2,6 +2,7 @@ package models_test
 
 import (
 	"fdsim/enums"
+	"fdsim/generators"
 	"fdsim/models"
 	"fdsim/utils"
 	"testing"
@@ -19,4 +20,15 @@ func TestTeamBuilder(t *testing.T) {
 	assert.Equal(t, 0.0, team.TransferRatio)
 	assert.Nil(t, team.Coach)
 	assert.NotNil(t, team.Roster)
+}
+
+func TestTeamAcceptingOffer(t *testing.T) {
+	tg := generators.NewTeamGen(0)
+	team := tg.Team(enums.EN)
+
+	p := team.Roster.Players()[0]
+	pVal := p.Value.Value()
+
+	acceptedPerc := team.AcceptsOffer(utils.NewEurosFromF(pVal-(pVal/2)), p.Id)
+	assert.Equal(t, 90, acceptedPerc.Val())
 }
